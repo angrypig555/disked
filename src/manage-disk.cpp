8b5a9ce006b8e550f29891a65a3e75d01f196c3e
angrypig555 <84266804+angrypig555@users.noreply.github.com>
2026-01-30 19:16:04 +0000

    add functionality to create mnt folder

--- src/manage-disk.cpp
+++ src/manage-disk.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<filesystem>
 #include"disks.hpp"
 #include"manage-disk.hpp"
 
@@ -27,4 +28,16 @@ int store_path(std::string path_to_disk, int drive) {
             break;
     }
     return 0;
+}
+
+int mount_disk(std::string drive, int num) {
+  std::string mnt_pathd = "/mnt/disked/";
+
+  if (std::filesystem::exists(mnt_pathd) && std::filesystem::is_directory(mnt_pathd)) {
+    std::cout << "[ok] disked mount found at /mnt/disked" << std::endl;
+  } else {
+    std::cout << "[warn] disked mount not found, creating at /mnt/disked/" << std::endl;
+    std::filesystem::create_directory("/mnt/disked/");
+  }
+  return 0;
 }
--- src/manage-disk.hpp
+++ src/manage-disk.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 int store_path(std::string path_to_disk, int drive);
+int mount_disk(std::string drive, int num);
