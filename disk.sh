e16f3759a022f71dc4ecbf49d3924aa56bd65b49
angrypig555 <84266804+angrypig555@users.noreply.github.com>
2026-01-31 10:08:02 +0000

    begin working on losetup script

---
+++ disk.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# fake disk generating software, use at own risk as sudo
+#
+set -e 
+
+echo "LAST CHANCE TO EXIT, MAKE SURE LOOP1-4 IS AVAILABLE, CONTIUNING IN 3 seconds"
+sleep 3
+
+echo "Creating disk images..."
+dd if=/dev/zero of=disk1.img bs=500M count=0 seek=1 
+dd if=/dev/zero of=disk2.img bs=500M count=0 seek=1 
+dd if=/dev/zero of=disk3.img bs=500M count=0 seek=1 
+dd if=/dev/zero of=disk4.img bs=500M count=0 seek=1 
+
+
